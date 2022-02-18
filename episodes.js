8b1c0b8e7b90eebfd84c35fc8432765fd0e785d4
mysteriouskd <99845500+mysteriouskd@users.noreply.github.com>
2022-02-18 05:32:25 -0300

    Add files via upload

---
+++ episodes.js
@@ -0,0 +1,1156 @@
+let s1= {
+E1 : {
+title : "The Evidence",
+date : "2010-04-20"
+},
+
+E2 : {
+title : "The Visitors",
+date : "2010-04-27"
+},
+
+E3 : {
+title : "The Mission",
+date : "2010-05-04"
+},
+
+E4 : {
+title : "Closer Encounters",
+date : "2010-05-18"
+},
+
+E5 : {
+title : "The Return",
+date : "2010-05-25"
+},
+
+SP1 : {
+title : "Special - Chariots, Gods & Beyond",
+date : "2009-03-08"
+}
+};
+
+let s2= {
+E1 : {
+title : "Mysterious Places",
+date : "2010-10-28"
+},
+
+E2 : {
+title : "Gods & Aliens",
+date : "2010-11-04"
+},
+
+E3 : {
+title : "Underwater Worlds",
+date : "2010-11-11"
+},
+
+E4 : {
+title : "Underground Aliens",
+date : "2010-11-18"
+},
+
+E5 : {
+title : "Aliens and the Third Reich",
+date : "2010-11-25"
+},
+
+E6 : {
+title : "Alien Tech",
+date : "2010-12-02"
+},
+
+E7 : {
+title : "Angels and Aliens",
+date : "2010-12-09"
+},
+
+E8 : {
+title : "Unexplained Structures",
+date : "2010-12-16"
+},
+
+E9 : {
+title : "Alien Devastations",
+date : "2010-12-23"
+},
+
+E10 : {
+title : "Alien Contacts",
+date : "2010-12-30"
+}
+};
+
+let s3= {
+E1 : {
+title : "Aliens and the Old West",
+date : "2011-07-28"
+},
+
+E2 : {
+title : "Aliens and Monsters",
+date : "2011-08-04"
+},
+
+E3 : {
+title : "Aliens and Sacred Places",
+date : "2011-08-11"
+},
+
+E4 : {
+title : "Aliens and Temples of Gold",
+date : "2011-08-18"
+},
+
+E5 : {
+title : "Aliens and Mysterious Rituals",
+date : "2011-08-25"
+},
+
+E6 : {
+title : "Aliens and Ancient Engineers",
+date : "2011-09-01"
+},
+
+E7 : {
+title : "Aliens, Plagues and Epidemics",
+date : "2011-09-08"
+},
+
+E8 : {
+title : "Aliens and Lost Worlds",
+date : "2011-09-15"
+},
+
+E9 : {
+title : "Aliens and Deadly Weapons",
+date : "2011-09-22"
+},
+
+E10 : {
+title : "Aliens and Evil Places",
+date : "2011-09-28"
+},
+
+E11 : {
+title : "Aliens and the Founding Fathers",
+date : "2011-10-05"
+},
+
+E12 : {
+title : "Aliens and Deadly Cults",
+date : "2011-10-12"
+},
+
+E13 : {
+title : "Aliens and the Secret Code",
+date : "2011-10-19"
+},
+
+E14 : {
+title : "Aliens and the Undead",
+date : "2011-10-26"
+},
+
+E15 : {
+title : "Aliens, Gods and Heroes",
+date : "2011-11-16"
+},
+
+E16 : {
+title : "Aliens and the Creation of Man",
+date : "2011-11-23"
+}
+};
+
+let s4= {
+E1 : {
+title : "The Mayan Conspiracy",
+date : "2012-02-17"
+},
+
+E2 : {
+title : "The Doomsday Prophecies",
+date : "2012-02-17"
+},
+
+E3 : {
+title : "The Greys",
+date : "2012-02-24"
+},
+
+E4 : {
+title : "Aliens and Mega-Disasters",
+date : "2012-03-02"
+},
+
+E5 : {
+title : "The NASA Connection",
+date : "2012-03-09"
+},
+
+E6 : {
+title : "The Mystery of Puma Punku",
+date : "2012-03-16"
+},
+
+E7 : {
+title : "Aliens and Bigfoot",
+date : "2012-03-23"
+},
+
+E8 : {
+title : "The Da Vinci Conspiracy",
+date : "2012-04-06"
+},
+
+E9 : {
+title : "The Time Travelers",
+date : "2012-04-27"
+},
+
+E10 : {
+title : "Aliens and Dinosaurs",
+date : "2012-05-04"
+}
+};
+
+let s5= {
+E1 : {
+title : "Secrets of the Pyramids",
+date : "2012-12-21"
+},
+
+E2 : {
+title : "Aliens and Cover-Ups",
+date : "2012-12-28"
+},
+
+E3 : {
+title : "Alien Power Plants",
+date : "2013-01-04"
+},
+
+E4 : {
+title : "Destination Orion",
+date : "2013-01-11"
+},
+
+E5 : {
+title : "The Einstein Factor",
+date : "2013-01-18"
+},
+
+E6 : {
+title : "Secrets of the Tombs",
+date : "2013-01-25"
+},
+
+E7 : {
+title : "Prophets and Prophecies",
+date : "2013-02-08"
+},
+
+E8 : {
+title : "Beyond Nazca",
+date : "2013-02-15"
+},
+
+E9 : {
+title : "Strange Abductions",
+date : "2013-02-22"
+},
+
+E10 : {
+title : "The von Däniken Legacy",
+date : "2013-04-05"
+},
+
+E11 : {
+title : "The Viking Gods",
+date : "2013-04-12"
+},
+
+E12 : {
+title : "The Monoliths",
+date : "2013-04-19"
+}
+};
+
+let s6= {
+E1 : {
+title : "The Power of Three",
+date : "2013-09-30"
+},
+
+E2 : {
+title : "The Crystal Skulls",
+date : "2013-10-07"
+},
+
+E3 : {
+title : "The Anunnaki Connection",
+date : "2013-10-14"
+},
+
+E4 : {
+title : "Magic of the Gods",
+date : "2013-10-21"
+},
+
+E5 : {
+title : "The Satan Conspiracy",
+date : "2013-10-28"
+},
+
+E6 : {
+title : "Alien Operations",
+date : "2013-11-01"
+},
+
+E7 : {
+title : "Emperors, Kings and Pharaohs",
+date : "2013-11-08"
+},
+
+E8 : {
+title : "Mysterious Relics",
+date : "2013-11-15"
+},
+
+E9 : {
+title : "Aliens and Forbidden Islands",
+date : "2013-11-29"
+},
+
+E10 : {
+title : "Aliens and the Lost Ark",
+date : "2013-12-06"
+},
+
+E11 : {
+title : "Aliens and Mysterious Mountains",
+date : "2013-12-13"
+}
+};
+
+let s7= {
+E1 : {
+title : "Aliens and Stargates",
+date : "2014-01-24"
+},
+
+E2 : {
+title : "Aliens in America",
+date : "2014-01-31"
+},
+
+E3 : {
+title : "The Star Children",
+date : "2014-02-07"
+},
+
+E4 : {
+title : "Treasures of the Gods",
+date : "2014-02-14"
+},
+
+E5 : {
+title : "Aliens and the Red Planet",
+date : "2014-02-21"
+},
+
+E6 : {
+title : "The Shamans",
+date : "2014-02-28"
+},
+
+E7 : {
+title : "Aliens and Insects",
+date : "2014-03-07"
+},
+
+E8 : {
+title : "Alien Breeders",
+date : "2014-03-14"
+}
+};
+
+let s8= {
+E1 : {
+title : "Alien Transports",
+date : "2014-06-13"
+},
+
+E2 : {
+title : "Mysterious Structures",
+date : "2014-06-20"
+},
+
+E3 : {
+title : "Mysterious Devices",
+date : "2014-06-27"
+},
+
+E4 : {
+title : "Faces of the Gods",
+date : "2014-07-25"
+},
+
+E5 : {
+title : "The Reptilians",
+date : "2014-07-25"
+},
+
+E6 : {
+title : "The Tesla Experiment",
+date : "2014-08-01"
+},
+
+E7 : {
+title : "The God Particle",
+date : "2014-08-08"
+},
+
+E8 : {
+title : "Alien Encounters",
+date : "2014-08-15"
+},
+
+E9 : {
+title : "Aliens & Superheroes",
+date : "2014-08-22"
+}
+};
+
+let s9= {
+E1 : {
+title : "Forbidden Caves",
+date : "2014-10-31"
+},
+
+E2 : {
+title : "Mysteries of the Sphinx",
+date : "2014-11-07"
+},
+
+E3 : {
+title : "Aliens Among Us",
+date : "2014-11-14"
+},
+
+E4 : {
+title : "The Genius Factor",
+date : "2014-11-21"
+},
+
+E5 : {
+title : "Secrets of the Mummies",
+date : "2014-11-28"
+},
+
+E6 : {
+title : "Alien Resurrections",
+date : "2014-12-05"
+},
+
+E7 : {
+title : "Alien Messages",
+date : "2014-12-19"
+},
+
+E8 : {
+title : "The Great Flood",
+date : "2014-12-23"
+},
+
+E9 : {
+title : "Aliens and the Civil War",
+date : "2015-04-10"
+},
+
+E10 : {
+title : "Hidden Pyramids",
+date : "2015-04-17"
+},
+
+E11 : {
+title : "The Vanishings",
+date : "2015-04-24"
+},
+
+E12 : {
+title : "The Alien Agenda",
+date : "2015-05-01"
+},
+
+SP1 : {
+title : "Special - The Ultimate Evidence: The Mystery of Puma Punku",
+date : "2015-06-12"
+},
+
+SP2 : {
+title : "Special - The Ultimate Evidence: The Monoliths",
+date : "2015-05-22"
+},
+
+SP3 : {
+title : "Special - The Ultimate Evidence: The Tesla Experiment",
+date : "2015-05-29"
+},
+
+SP4 : {
+title : "Special - The Ultimate Evidence: Aliens and the Lost Ark",
+date : "2015-06-26"
+},
+
+SP5 : {
+title : "Special - The Ultimate Evidence: Alien Power Plants",
+date : "2015-06-05"
+},
+
+SP6 : {
+title : "Special - The Ultimate Evidence: Beyond Nazca",
+date : "2015-05-15"
+},
+
+SP7 : {
+title : "Special - The Ultimate Evidence: Alien Messages",
+date : "2015-06-19"
+}
+};
+
+let s10= {
+E1 : {
+title : "Aliens B.C.",
+date : "2015-07-24"
+},
+
+E2 : {
+title : "NASA's Secret Agenda",
+date : "2015-07-31"
+},
+
+E3 : {
+title : "Aliens and Robots",
+date : "2015-08-07"
+},
+
+E4 : {
+title : "Dark Forces",
+date : "2015-08-14"
+},
+
+E5 : {
+title : "The Alien Evolution",
+date : "2015-08-21"
+},
+
+E6 : {
+title : "The Other Earth",
+date : "2015-08-28"
+},
+
+E7 : {
+title : "Creatures of the Deep",
+date : "2015-09-04"
+},
+
+E8 : {
+title : "Circles from the Sky",
+date : "2015-09-18"
+},
+
+E9 : {
+title : "The Alien Wars",
+date : "2015-10-02"
+},
+
+E10 : {
+title : "The Forbidden Zones",
+date : "2015-10-09"
+},
+
+SP1 : {
+title : "Special - The Ultimate Evidence: Aliens and the Creation of Man",
+date : "2015-07-24"
+}
+};
+
+let s11= {
+E1 : {
+title : "Pyramids of Antarctica",
+date : "2016-05-06"
+},
+
+E2 : {
+title : "Destination Mars",
+date : "2016-05-13"
+},
+
+E3 : {
+title : "The Next Humans",
+date : "2016-05-20"
+},
+
+E4 : {
+title : "The New Evidence",
+date : "2016-05-27"
+},
+
+E5 : {
+title : "The Visionaries",
+date : "2016-06-10"
+},
+
+E6 : {
+title : "Decoding the Cosmic Egg",
+date : "2016-06-17"
+},
+
+E7 : {
+title : "The Wisdom Keepers",
+date : "2016-06-24"
+},
+
+E8 : {
+title : "The Mysterious Nine",
+date : "2016-07-08"
+},
+
+E9 : {
+title : "The Hidden Empire",
+date : "2016-07-15"
+},
+
+E10 : {
+title : "The Prototypes",
+date : "2016-07-22"
+},
+
+E11 : {
+title : "Space Station Moon",
+date : "2016-07-29"
+},
+
+E12 : {
+title : "Russia's Secret Files",
+date : "2016-08-12"
+},
+
+E13 : {
+title : "Beyond Roswell",
+date : "2016-08-19"
+},
+
+E14 : {
+title : "The Returned",
+date : "2016-08-26"
+},
+
+E15 : {
+title : "Shiva the Destroyer",
+date : "2016-09-02"
+}
+};
+
+let s12= {
+E1 : {
+title : "The Alien Hunters",
+date : "2017-04-28"
+},
+
+E2 : {
+title : "Forged by the Gods",
+date : "2017-05-05"
+},
+
+E3 : {
+title : "The Mystery of Rudloe Manor",
+date : "2017-05-12"
+},
+
+E4 : {
+title : "The Alien Architects",
+date : "2017-05-19"
+},
+
+E5 : {
+title : "The Pharaohs' Curse",
+date : "2017-05-26"
+},
+
+E6 : {
+title : "The Science Wars",
+date : "2017-06-02"
+},
+
+E7 : {
+title : "City of the Gods",
+date : "2017-06-09"
+},
+
+E8 : {
+title : "The Alien Frequency",
+date : "2017-06-16"
+},
+
+E9 : {
+title : "The Majestic Twelve",
+date : "2017-07-07"
+},
+
+E10 : {
+title : "The Akashic Record",
+date : "2017-07-14"
+},
+
+E11 : {
+title : "Voices of the Gods",
+date : "2017-07-21"
+},
+
+E12 : {
+title : "The Animal Agenda",
+date : "2017-07-28"
+},
+
+E13 : {
+title : "The Replicants",
+date : "2017-08-04"
+},
+
+E14 : {
+title : "A Spaceship Made of Stone",
+date : "2017-08-11"
+},
+
+E15 : {
+title : "The Alien Disks",
+date : "2017-09-08"
+},
+
+E16 : {
+title : "Return to Gobekli Tepe",
+date : "2017-09-15"
+}
+};
+
+let s13= {
+E1 : {
+title : "The UFO Conspiracy",
+date : "2018-04-27"
+},
+
+E2 : {
+title : "Da Vinci's Forbidden Codes",
+date : "2018-05-04"
+},
+
+E3 : {
+title : "The Alien Protocols",
+date : "2018-05-11"
+},
+
+E4 : {
+title : "Earth's Black Holes",
+date : "2018-05-18"
+},
+
+E5 : {
+title : "The Desert Codes",
+date : "2018-05-25"
+},
+
+E6 : {
+title : "Area 52",
+date : "2018-06-01"
+},
+
+E7 : {
+title : "Earth Station Egypt",
+date : "2018-07-20"
+},
+
+E8 : {
+title : "Island of the Giants",
+date : "2018-07-27"
+},
+
+E9 : {
+title : "The Taken",
+date : "2018-08-03"
+},
+
+E10 : {
+title : "The Sentinels",
+date : "2018-08-10"
+},
+
+E11 : {
+title : "Russia Declassified",
+date : "2018-08-17"
+},
+
+E12 : {
+title : "They Came from the Sky",
+date : "2018-08-24"
+},
+
+E13 : {
+title : "The Artificial Human",
+date : "2018-08-31"
+},
+
+E14 : {
+title : "The Alien Phenomenon",
+date : "2019-01-04"
+},
+
+E15 : {
+title : "Return to Mars",
+date : "2019-01-07"
+}
+};
+
+let s14= {
+E1 : {
+title : "Return to Antarctica",
+date : "2019-05-31"
+},
+
+E2 : {
+title : "The Badlands Guardian",
+date : "2019-06-07"
+},
+
+E3 : {
+title : "Element 115",
+date : "2019-06-14"
+},
+
+E4 : {
+title : "The Star Gods of Sirius",
+date : "2019-06-21"
+},
+
+E5 : {
+title : "They Came from the Sea",
+date : "2019-06-28"
+},
+
+E6 : {
+title : "Secrets of the Maya",
+date : "2019-07-05"
+},
+
+E7 : {
+title : "The Druid Connection",
+date : "2019-07-19"
+},
+
+E8 : {
+title : "The Reptilian Agenda",
+date : "2019-07-26"
+},
+
+E9 : {
+title : "The Alien Infection",
+date : "2019-08-02"
+},
+
+E10 : {
+title : "Project Hybrid",
+date : "2019-08-09"
+},
+
+E11 : {
+title : "The Trans-Dimensionals",
+date : "2019-08-16"
+},
+
+E12 : {
+title : "Islands of Fire",
+date : "2019-08-23"
+},
+
+E13 : {
+title : "The Constellation Code",
+date : "2019-08-30"
+},
+
+E14 : {
+title : "The Nuclear Agenda",
+date : "2019-09-06"
+},
+
+E15 : {
+title : "The Alien Mountain",
+date : "2019-10-04"
+},
+
+E16 : {
+title : "The Alien Brain",
+date : "2019-10-11"
+},
+
+E17 : {
+title : "The Secrets of Stonehenge",
+date : "2019-10-18"
+},
+
+E18 : {
+title : "Food of the Gods",
+date : "2019-11-01"
+},
+
+E19 : {
+title : "Human Hieroglyphs",
+date : "2019-11-08"
+},
+
+E20 : {
+title : "The Storming of Area 51",
+date : "2019-11-15"
+},
+
+E21 : {
+title : "Countdown to Disclosure",
+date : "2019-11-22"
+},
+
+E22 : {
+title : "Secrets of the Exoplanets",
+date : "2019-11-29"
+}
+};
+
+let s15= {
+E1 : {
+title : "The Mystery of Nan Madol",
+date : "2020-01-25"
+},
+
+E2 : {
+title : "The Relics of Roswell",
+date : "2020-02-01"
+},
+
+E3 : {
+title : "Destination Chile",
+date : "2020-02-08"
+},
+
+E4 : {
+title : "The Real Men in Black",
+date : "2020-02-15"
+},
+
+E5 : {
+title : "The Mystery of the Stone Giants",
+date : "2020-02-22"
+},
+
+E6 : {
+title : "The World Before Time",
+date : "2020-02-29"
+},
+
+E7 : {
+title : "They Came from the Pleiades",
+date : "2020-03-07"
+},
+
+E8 : {
+title : "The Immortality Machine",
+date : "2020-03-14"
+},
+
+E9 : {
+title : "The Shapeshifters",
+date : "2020-03-21"
+},
+
+E10 : {
+title : "The Mystery of Skinwalker Ranch",
+date : "2020-03-28"
+},
+
+E11 : {
+title : "The Ultimate Guide to UFO's",
+date : "2020-04-11"
+},
+
+E12 : {
+title : "Aliens and the Presidents",
+date : "2020-04-18"
+}
+};
+
+let s16= {
+E1 : {
+title : "The Divine Number",
+date : "2020-11-13"
+},
+
+E2 : {
+title : "The Lost Kingdom",
+date : "2020-11-20"
+},
+
+E3 : {
+title : "The Galactic Keyhole",
+date : "2020-12-04"
+},
+
+E4 : {
+title : "Giants of the Mediterranean",
+date : "2020-12-11"
+},
+
+E5 : {
+title : "The Forbidden Bible",
+date : "2020-12-18"
+},
+
+E6 : {
+title : "William Shatner Meets Ancient Aliens",
+date : "2021-02-12"
+},
+
+E7 : {
+title : "Impossible Artifacts",
+date : "2021-02-19"
+},
+
+E8 : {
+title : "The Space Travelers",
+date : "2021-02-26"
+},
+
+E9 : {
+title : "UFO Pioneers",
+date : "2021-03-05"
+},
+
+E10 : {
+title : "The Harmonic Code",
+date : "2021-03-12"
+}
+};
+
+let s17= {
+E1 : {
+title : "The Lost City of Peru",
+date : "2021-08-06"
+},
+
+E2 : {
+title : "Top Ten Mysterious Sites",
+date : "2021-08-13"
+},
+
+E3 : {
+title : "Top Ten Alien Cover-Ups",
+date : "2021-08-20"
+},
+
+E4 : {
+title : "The Mystery of Mount Shasta",
+date : "2021-09-17"
+},
+
+E5 : {
+title : "The Human Experiment",
+date : "2021-09-24"
+},
+
+E6 : {
+title : "The Alien Shape",
+date : "2021-09-10"
+},
+
+E7 : {
+title : "Top Ten Alien Artifacts",
+date : "2021-10-08"
+}
+};
+
+let s18= {
+E1 : {
+title : "The Disclosure Event",
+date : "2022-01-07"
+},
+
+E2 : {
+title : "Mystery of the Standing Stones",
+date : "2022-01-14"
+},
+
+E3 : {
+title : "Beneath the Sacred Temples",
+date : "2022-01-21"
+},
+
+E4 : {
+title : "The World on Alert",
+date : "2022-01-28"
+},
+
+E5 : {
+title : "Recovering the Ark of the Covenant",
+date : "2022-02-11"
+},
+
+E6 : {
+title : "Secrets of the Star Ancestors",
+date : "2022-02-18"
+},
+
+E7 : {
+title : "Alien Air Force",
+date : "2022-02-25"
+},
+
+E8 : {
+title : "The Shadow People",
+date : "2022-03-04"
+},
+
+E9 : {
+title : "Decoding The Dragon Gods",
+date : "2022-03-11"
+},
+
+E10 : {
+title : "The Time Benders",
+date : "2022-03-18"
+}
+};
+
+
+
+function makeTables(a,obj){
+for (let key in obj) {
+if(obj.hasOwnProperty(key)){
+let tr= `<tr>
+<td>${key}</td>
+<td>${obj[key].title}</td>
+<td>${obj[key].date}</td>
+</tr>`;
+
+a.innerHTML+= tr;
+}}
+
+};
+
+
+let allseasons= [s1,s2,s3,s4,s5,s6,s7,s8,s9,s10,s11,s12,s13,s14,s15,s16,s17,s18];
+
+
+allseasons.forEach(i=>{
+let a= create("table");
+select("#container").append(a);
+makeTables(a,i);
+});
